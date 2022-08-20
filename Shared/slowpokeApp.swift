b639e69039c7b45599b4c70a69bde7a567499279
Giovani Schiar <giovanischiar@gmail.com>
2022-08-20 02:46:41 -0700

    Initial Commit

---
+++ Shared/slowpokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  slowpokeApp.swift
+//  Shared
+//
+//  Created by Giovani Schiar on 20/08/22.
+//
+
+import SwiftUI
+
+@main
+struct slowpokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
